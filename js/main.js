110d464264f3e414f1c4c54b3a0e41083fb122e7
SaemJeon <38971167+saemjeon@users.noreply.github.com>
2021-05-18 22:58:53 -0400

    Completed rewards section

--- js/main.js
+++ js/main.js
@@ -21,8 +21,36 @@ fadeInEls.forEach((fadeInEl, index) => {
   });
 });
 
-new Swiper(".notice .swiper-container", {
+new Swiper(".notice-line .swiper-container", {
   direction: "vertical",
   loop: true,
   autoplay: true,
+});
+
+new Swiper(".promotion .swiper-container", {
+  slidesPerView: 3,
+  centeredSlides: true,
+  spaceBetween: 10,
+  loop: true,
+  autoplay: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+});
+
+const promoButton = document.querySelector(".promo-expand");
+const promotionEl = document.querySelector(".promotion");
+let hidePromotion = true;
+promoButton.addEventListener("click", () => {
+  hidePromotion = !hidePromotion;
+  if (hidePromotion) {
+    promotionEl.classList.add("hide");
+  } else {
+    promotionEl.classList.remove("hide");
+  }
 });
